eb624d474ae2c3f72081efcc9c331ee7a211c73f
Joel Graf <joel.graf1@swisscom.com>
2021-12-25 16:24:54 +0100

    feat: added day 4 of 2015

---
+++ 2015/4/part_2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const k = fs.readFileSync("input.txt", "utf-8");
+let a = 0;
+let h = "";
+while (!h.startsWith("000000")) {
+    a++;
+    h = crypto.createHash("md5").update(`${k}${a}`).digest("hex");
+}
+console.log(a);
