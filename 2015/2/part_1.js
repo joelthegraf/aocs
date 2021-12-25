a2599ee5dcd2292d64f280ba0920549cd5ec891d
Joel Graf <joel.graf1@swisscom.com>
2021-12-25 16:24:18 +0100

    feat: added day 2 of 2015

---
+++ 2015/2/part_1.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let t = 0;
+fs.readFileSync("input.txt", "utf-8")
+    .split("\n")
+    .forEach(p => {
+        const [l, w, h] = p.split("x");
+        const [lw, wh, hl] = [l * w, w * h, h * l];
+        t += 2 * lw + 2 * wh + 2 * hl + Math.min(lw, wh, hl);
+    })
+console.log(t);
---
+++ 2015/2/part_2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let t = 0;
+fs.readFileSync("input.txt", "utf-8")
+    .split("\n")
+    .forEach(p => {
+        const [l, w, h] = p.split("x");
+        const [ll, ww, hh] = [2 * l, 2 * w, 2 * h];
+        t += Math.min(hh + ww, hh + ll, ww + ll) + l * w * h;
+    })
+console.log(t);
