82c6c30ea8b496c456e9baf2989d18dde8c2e8d3
Joel Graf <joel.graf1@swisscom.com>
2021-12-25 16:25:38 +0100

    feat: added day 6 of 2015

---
+++ 2015/6/part_1.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+let g = Array.from(Array(1000), () => new Array(1000).fill(false));
+fs.readFileSync("input.txt", "utf-8")
+    .split("\n")
+    .forEach(i => {
+        const m = i.match(/([^\d]+)(\d+,\d+)[^\d]+(\d+,\d+)/);
+        const a = m[1].trim();
+        const [x1, y1] = m[2].split(",");
+        const [x2, y2] = m[3].split(",");
+        for (let x = parseInt(x1); x <= parseInt(x2); x++) {
+            for (let y = parseInt(y1); y <= parseInt(y2); y++) {
+                switch (a) {
+                    case "turn on":
+                        g[y][x] = true;
+                        break;
+                    case "turn off":
+                        g[y][x] = false;
+                        break;
+                    case "toggle":
+                        g[y][x] = !g[y][x];
+                        break;
+                }
+            }
+        }
+    });
+
+let c = 0;
+for (let x = 0; x < 1000; x++) {
+    for (let y = 0; y < 1000; y++) {
+        g[y][x] && c++;
+    }
+}
+console.log(c);
