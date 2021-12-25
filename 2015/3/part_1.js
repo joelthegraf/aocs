e9f36402f83f1f715ee8fcf1f6d2a405d2a1d3a5
Joel Graf <joel.graf1@swisscom.com>
2021-12-25 16:24:36 +0100

    feat: added day 3 of 2015

---
+++ 2015/3/part_1.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+let x = 0, y = 0;
+let g = [`${x} ${y}`]
+fs.readFileSync("input.txt", "utf-8")
+    .split("")
+    .forEach(i => {
+        switch (i) {
+            case "^":
+                y++;
+                break;
+            case "v":
+                y--;
+                break;
+            case ">":
+                x++;
+                break;
+            case "<":
+                x--;
+                break;
+        }
+        g.push(`${x} ${y}`)
+    });
+console.log([...new Set(g)].length);
