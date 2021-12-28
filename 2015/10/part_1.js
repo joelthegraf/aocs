54c5a2f7d33150a3efd5f0eb4f3a49b687f5f12e
Joel Graf <graf.joel03@gmail.com>
2021-12-28 12:44:41 +0100

    feat: added day 10 of 2015

---
+++ 2015/10/part_1.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const las = (s) => {
+  let r = [];
+  let i = 0;
+  while (i < s.length) {
+    let c = 1;
+    while (i + 1 < s.length && s[i] === s[i + 1]) {
+      i++;
+      c++;
+    }
+    r.push(`${c}${s[i]}`);
+    i++;
+  }
+  return r.join("");
+};
+
+let l = fs.readFileSync("input.txt", "utf-8");
+for (let i = 0; i < 40; i++) {
+  l = las(l);
+}
+console.log(l.length);
